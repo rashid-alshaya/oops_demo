b62181caa796973c1021dc1045d5ca2874d572e9
Mohd Rashid <mohd_rashid_123@yahoo.com>
2017-11-29 16:14:25 +0530

    changes made in index.php

--- index.php
+++ index.php
@@ -5,52 +5,19 @@
  * contains the TextService class to get the calculated result.
  */
 
-require_once 'Text.php';
-require_once 'TextCalculate.php';
+require_once 'src/Classes/Text.php';
+require_once 'src/Classes/TextCalculate.php';
+require_once 'src/Classes/Validate.php';
+require_once 'src/Classes/Format.php';
+require_once 'src/Classes/Output.php';
 
-class TextService
-{
-  /*
-   * contains the object of TextCalculate class.
-   * @var type
-   */
-  private $textCalculateService;
-  
-  /**
-   * Contains and assign the textCalculate and Text class object into class variables.
-   * @param TextCalculateInterface $textCalculate
-   * @param TextInterface $textServ
-   */
-  public function __construct(TextCalculateInterface $textCalculate){    
-    $this->textCalculateService = $textCalculate;
-  }
-  
-  /**
-   * 
-   * @param type $text - contains string 
-   * @param array $stopWords - contains words to be removed from the string. 
-   * @param type $type - contains the format for content type.
-   * @return type - returns the results in the form of json or array.
-   */
-  public function getResults($text, array $stopWords, $type){
-    return $this->textCalculateService->calculateText($text, $stopWords, $type);   
-  }
-}
 
 /**
- * contains the string and arr as setter and getter
+ * contains the result as response in the form of array, json or xml.
+ * @var output
  */
-$text = new Text();
-$text->setType('arr');
-$text->setText('Lorem ipsum dolor aa aa s$it amet ex bb his altera aa latine bb');
-$text->setStopWords(array('Lorem','his','latine'));
+$output = new Output(new Text('Lorem ipsum dolor aa aa sit amet ex bb his altera aa latine bb', array('Lorem','his','latine'),'arr','en'), new Validate(), new TextCalculate, new Format());
 
-/**
- * contains the result as response in the form of array or json.
- * @var str
- */
-$str = new TextService(new TextCalculate());
+$result = $output->getOutput();
 
-$data = $str->getResults($text->getText(),$text->getStopWords() , $text->getType());
-
-print "<pre>";print_r($data);die;
+print "<pre>"; print_r($result);die;
